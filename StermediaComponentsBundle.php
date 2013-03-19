45bb6fe4a05658b2caccffc4ff6009d769721834
Jakub Paszkiewicz <j.paszkiewicz@stermedia.pl>
2013-03-19 12:03:46 +0100

    created bundle class

---
+++ StermediaComponentsBundle.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Stermedia\StermediaComponentsBundle
+ *
+ * (c) Stermedia <http://stermedia.eu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Stermedia\CommonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Stermedia Common bundle
+ *
+ * @author  Jakub Paszkiewicz <paszkiewicz.jakub@gmail.com>
+ */
+class StermediaCommonBundle extends Bundle
+{
+}
